63417b6890c4187125aaae412538390e518bb74e
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 09:29:26 -0700

    create note element

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ window.onload = function () {
     notes.forEach((note) => createNoteElement(note));
   };
   
+  // Function to create note element
+  function createNoteElement(note) {
+    const noteDiv = document.createElement("div");
+    noteDiv.classList.add("note");
+    noteDiv.style.backgroundColor = note.color;
+    noteDiv.setAttribute("draggable", "true");
+    
+    noteDiv.innerHTML = `
+      <textarea>${note.content}</textarea>
+      <div class="note-footer">
+        <input type="color" value="${note.color}" class="color-picker" />
+        <button class="delete-note">❌</button>
+      </div>
+    `;
+  
+  
